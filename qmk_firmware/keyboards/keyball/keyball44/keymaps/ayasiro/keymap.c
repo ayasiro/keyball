7b9a624389b792145205ef6cfe0b79c9ee805deb
user <user@M2-Macbook-Air.local>
2024-12-05 13:36:44 +0900

    commit

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/ayasiro/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/ayasiro/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV   , KC_Q         , KC_W         , KC_E     , KC_R            , KC_T       ,                                      KC_Y        , KC_U           , KC_I     , KC_O         , KC_P             , KC_EQUAL ,
     KC_LBRC  , LCTL_T(KC_A) , LSFT_T(KC_S) , KC_D     , KC_F            , KC_G       ,                                      KC_H        , KC_J           , KC_K     , RSFT_T(KC_L) , RCTL_T(KC_SCLN)  , KC_QUOTE ,
     KC_RBRC  , KC_Z         , KC_X         , KC_C     , KC_V            , KC_B       ,                                      KC_N        , KC_M           , KC_COMM  , KC_DOT       , KC_MINUS         , KC_SLASH ,
-                              KC_LALT      , KC_LGUI  , ________        , LSFT_T(KC_SPC), _______ ,                  KC_BSPC, LT(1,KC_ENT), XXXXXXX        , XXXXXXX  , KC_BSLS	
+                              KC_LALT      , KC_LGUI  , _______         , LSFT_T(KC_SPC), _______ ,                  KC_BSPC, LT(1,KC_ENT), XXXXXXX        , XXXXXXX  , KC_BSLS	
   ),
 
   [1] = LAYOUT_universal(
